385acff68e21e64da3cd14d1dff60d9e8c99ee98
jumelet <jumeletjaap@gmail.com>
2020-09-23 18:05:39 +0200

    Add experiment scripts

---
+++ explain.py
@@ -0,0 +1,15 @@
+from diagnnose.attribute.explainer import Explainer
+from diagnnose.config.config_dict import create_config_dict
+from diagnnose.models import LanguageModel
+from diagnnose.models.import_model import import_model
+from diagnnose.tokenizer import create_tokenizer
+
+if __name__ == "__main__":
+    config_dict = create_config_dict()
+
+    model: LanguageModel = import_model(config_dict)
+    tokenizer = create_tokenizer(**config_dict["tokenizer"])
+
+    explainer = Explainer(model, tokenizer, decomposer="shapley_decomposer")
+
+    explainer.explain(["The athletes above Barbara <mask>."], ["Ġapprove", "Ġapproves"])
